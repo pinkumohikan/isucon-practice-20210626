0c8ece59d661a5259a5f2d83099d136638b33364
masami <msihima0304@gmail.com>
2021-06-26 15:28:49 +0900

    feat: 🎸 画像書き出し

--- webapp/golang/saveImage.go
+++ webapp/golang/saveImage.go
@@ -10,27 +10,11 @@ import (
 )
 
 func main() {
-	host := os.Getenv("ISUCONP_DB_HOST")
-	if host == "" {
-		host = "localhost"
-	}
-	port := os.Getenv("ISUCONP_DB_PORT")
-	if port == "" {
-		port = "3306"
-	}
-	_, err := strconv.Atoi(port)
-	if err != nil {
-		log.Fatalf("Failed to read DB port number from an environment variable ISUCONP_DB_PORT.\nError: %s", err.Error())
-	}
-	user := os.Getenv("ISUCONP_DB_USER")
-	if user == "" {
-		user = "root"
-	}
-	password := os.Getenv("ISUCONP_DB_PASSWORD")
-	dbname := os.Getenv("ISUCONP_DB_NAME")
-	if dbname == "" {
-		dbname = "isuconp"
-	}
+	host := "isucon-app3"
+	port := "3306"
+	user := "isuconp"
+	password := "isuconp"
+	dbname := "isuconp"
 
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
@@ -41,7 +25,7 @@ func main() {
 		dbname,
 	)
 
-	db, err = sqlx.Open("mysql", dsn)
+	db, err := sqlx.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Failed to connect to DB: %s.", err.Error())
 	}
