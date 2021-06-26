eabfc766e6f2723e23d30440ef88b71e620a851b
masami <msihima0304@gmail.com>
2021-06-26 16:50:31 +0900

    feat: 🎸 適当な値を入れてみる

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -685,7 +685,7 @@ func postIndex(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	query := "INSERT INTO `posts` (`user_id`, `mime`, `body`) VALUES (?,?,?)"
+	query := "INSERT INTO `posts` (`user_id`, `mime`, `body`,`Imgdata`) VALUES (?,?,?, 'aaa')"
 	result, err := db.Exec(
 		query,
 		me.ID,
