404195e9c1f5c7c0e32ef177523ff7c3d493d02f
masami <msihima0304@gmail.com>
2021-06-26 14:26:48 +0900

    feat: 🎸 返す投稿数の調整

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -424,7 +424,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC LIMIT 21")
+	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC LIMIT 20")
 	if err != nil {
 		log.Print(err)
 		return
